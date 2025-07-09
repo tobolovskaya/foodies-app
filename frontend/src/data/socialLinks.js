5b84cb7f2625644ae68ee5c20fcfe6d29a76b20b
Yevheniia Tobolovska <tobolovskaya@gmail.com>
2025-07-09 07:41:16 +0200

    Update socialLinks.js

--- frontend/src/data/socialLinks.js
+++ frontend/src/data/socialLinks.js
@@ -2,17 +2,17 @@ import icons from '../icons/sprite.svg';
 
 const links = [
   {
-    href: 'https://www.facebook.com/goITclub/',
+    href: 'https://www.facebook.com',
     icon: `${icons}#facebook`,
     label: 'Facebook',
   },
   {
-    href: 'https://www.instagram.com/goitclub/',
+    href: 'https://www.instagram.com',
     icon: `${icons}#instagram`,
     label: 'Instagram',
   },
   {
-    href: 'https://www.youtube.com/c/GoIT',
+    href: 'https://www.youtube.com',
     icon: `${icons}#youtube`,
     label: 'YouTube',
   },
