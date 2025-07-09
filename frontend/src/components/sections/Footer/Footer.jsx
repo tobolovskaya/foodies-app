3d1e97f0734d083c39a66970cad727defdf7356b
Yevheniia Tobolovska <tobolovskaya@gmail.com>
2025-07-09 07:48:15 +0200

    Update Footer.jsx

--- frontend/src/components/sections/Footer/Footer.jsx
+++ frontend/src/components/sections/Footer/Footer.jsx
@@ -1,21 +1,13 @@
-import CircularText from '../../CircularText/CircularText';
+
 import Logo from '../../Logo/Logo';
 import NetworkLinks from '../../NetworkLinks/NetworkLinks';
 import styles from './Footer.module.css';
 
-const Footer = ({ openModal }) => {
+const Footer = () => {
   return (
     <footer className={`container ${styles.footer}`}>
       <div className={styles.wrapper}>
         <Logo />
-        <div onClick={() => openModal('team')}>
-          <CircularText
-            text="SPACE*RACCOONS*TEAM*"
-            onHover="speedUp"
-            spinDuration={20}
-            className="custom-class"
-          />
-        </div>
         <NetworkLinks />
       </div>
       <p className={styles.copyright}>
